8616891d07274d988677913ba22acaf6bf9ee679
Ariel Cercado <acercado@risingtide.ph>
2015-10-06 18:00:05 +0800

    initial

---
+++ auth3rd/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+
+# def home(request):
+# 	context = RequestContext(request, {'user': request.user})
+# 	return render_to_response('auth3rd/home.html', context_instance=context)
+
+def home(request):
+   context = RequestContext(request, {'request': request,'user': request.user})
+   return render_to_response('auth3rd/home.html', context_instance=context)
